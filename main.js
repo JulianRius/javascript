2fb07bc14e2e103e6cddfbffa787fdad986a8c9f
Julian <julianrius@gmail.com>
2022-04-17 20:16:52 -0300

    reparación final versión original

--- main.js
+++ main.js
@@ -4,13 +4,12 @@ const sueldo = () => {
     do {
         sueldoBruto = parseFloat(prompt("Ingrese su sueldo bruto"));
     } while (sueldoBruto <= 0 || isNaN(sueldoBruto) || sueldoBruto === " ");
-    alert("Su sueldo ingresado es :" + " " + sueldoBruto);
     return sueldoBruto;
 };
 
 const condicionConvenio = () => {
 
-    let convenio = prompt("Ingrese su situación labora\n1)BAJO CONVENIO\n2)FUERA DE CONVENIO");
+    let convenio = 0
 
     while (convenio > 2 || convenio < 1 || isNaN(convenio)) {
         convenio = prompt("Ingrese su situación laboral\n1)BAJO CONVENIO\n2)FUERA DE CONVENIO");
@@ -18,81 +17,79 @@ const condicionConvenio = () => {
     return (convenio)
 };
 
-const descuento = (aportes) => {
-    if (aportes == 1) {
+const descuento = (dentroDeConvenio) => {
+
+    if (dentroDeConvenio) {
+        
         return 0.795;
     } else {
         return 0.83;
     };
 };
 
-const sueldoNeto = (situacion, aportes, haberes) => {
 
-    if (situacion == 1) {
-        alert("Su sueldo neto es " + "$" + haberes * aportes)
-        return (parseFloat(haberes * aportes));
-    } else {
-        alert("Su sueldo neto es " + "$" + haberes * aportes)
-        return parseFloat((haberes * aportes));
-    }
-};
 
-const ganancias = (haberesSretencion) => {
+const ganancias = (haberesSinAportes) => {
 
     let iigg = 0.35;
     let iiggMedium = 0.20;
 
-    if (haberesSinAportes > 150000) {
-        alert("Su impuesto es $" + "" + haberesSinAportes * iigg);
+    if (haberesSinAportes >= 150000) {
         return iigg;
-    } else if (haberesSinAportes <= 149999 && haberesSinAportes > 100000) {
-        alert("Su impuesto es $" + "" + haberesSinAportes * iiggMedium);
+    } else if (haberesSinAportes >= 100000) {
         return iiggMedium;
     } else {
-        alert("Ud.no debe pagar IIGG");
+        return 0.0;
     }
 };
 
-const cierre = (impuesto, limpio) => {
+const cierre = (impGanancias, haberesSinAportes) => {
 
-    let sFinal = haberesSinAportes - (impGanancias * haberesSinAportes);
-
-    while (impuesto != "") {
-        let sFinal = haberesSinAportes - (impGanancias * haberesSinAportes);
-        if (isNaN(sFinal) || haberesSinAportes < 100000) {
-            alert("Su sueldo final será $" + "" + haberesSinAportes)
-            break
-        } else {}
-        alert("Su sueldo final será $" + "" + sFinal);
-        break;
-    }
+    let sFinal = haberesSinAportes * (1 - impGanancias);
     return sFinal;
 };
 
-const continua =()=> {
-let avanzar = confirm ("¿Desea hacer otra consulta?")
+const continua = () => {
+    let avanzar = confirm("¿Desea hacer otra consulta?")
 
-if(avanzar===true){
- alert("Si desea realizar otra consulta presione F5 y reinicie el simulador")
-}else{
-    alert("Muchas gracias por utilizar nuestro servicio");
+    if (!avanzar) {
+        alert("Muchas gracias por utilizar nuestro servicio");
+    }
+    return avanzar;
 };
 
+
+
+
+
+const liquidar = () => {
+    let sueldoBrutoIngresado = sueldo();
+    console.log(sueldoBrutoIngresado);
+
+    //1-BAJO CONVENIO 2 FUERA DE CONVENIO
+
+    let situacionConvencional = condicionConvenio();
+    console.log(situacionConvencional);
+    let importeDescuento = descuento(situacionConvencional == 1);
+    console.log(importeDescuento);
+    let haberesSinAportes = sueldoBrutoIngresado * importeDescuento;
+    console.log(haberesSinAportes);
+    
+    //alert("Su sueldo neto es $" +""+ haberesSinAportes);
+    let impGanancias = ganancias(haberesSinAportes); 
+    //alert("Su impuesto es $" + "" + haberesSinAportes * impGanancias);
+    console.log(impGanancias);
+    let sueldoFinal = cierre(impGanancias, haberesSinAportes);
+    //alert("Su sueldo final será $" + "" + sueldoFinal);
+    alert("Sueldo bruto: $"+""+sueldoBrutoIngresado+"\n"+
+    "Dentro de Convenio: "+""+situacionConvencional+"\n"+
+    "Sueldo neto s/retención: $"+""+haberesSinAportes+"\n"+
+    "% IIGG:"+""+impGanancias+"\n"+
+    "Su sueldo final será: $"+""+sueldoFinal);
+    console.log(sueldoFinal);
+};
+
+do {
+    liquidar()
 }
-
-
-
-let sueldoBrutoIngresado = sueldo();
-console.log(sueldoBrutoIngresado);
-let situacionConvencional = condicionConvenio();
-console.log(situacionConvencional);
-let importeDescuento = descuento(situacionConvencional);
-console.log(importeDescuento);
-let haberesSinAportes = sueldoNeto(situacionConvencional, importeDescuento, sueldoBrutoIngresado);
-console.log(haberesSinAportes);
-let impGanancias = ganancias(haberesSinAportes);
-console.log(impGanancias);
-let sueldoFinal = cierre(impGanancias);
-console.log(sueldoFinal);
-let avanzar = continua()
-console.log(avanzar);
+while (continua());
